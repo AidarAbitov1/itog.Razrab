f69e16183b3b5a26aa630b39b1ab8eeb648fb53f
@Aidar Abitov <aydar099@gmail.com>
2023-09-14 11:40:35 +0300

    shag 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿sing System;
+using static System.Console;
+
+string[]array = {"Hello,",  "2,",  "world,",   "1234,",     "1567,", "123,", "qws", "+_)", "computer science,"};       
+WriteLine();  //Создаём массив строк array содердажащий некоторые значения
+MethodCreateInputArray(array); //Выводим метод первый 
+WriteLine();
+MethodCreateOutputArray(array);//Выводим метод второй 
+WriteLine();//Результат работы выводится в консоль при dotnet new console 
