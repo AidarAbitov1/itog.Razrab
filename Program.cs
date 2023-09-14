546cc29c2cbe61477c8f920767c06d1bf9bb664e
@Aidar Abitov <aydar099@gmail.com>
2023-09-14 11:42:18 +0300

    git commit -m

--- Program.cs
+++ Program.cs
@@ -7,3 +7,13 @@ MethodCreateInputArray(array); //Выводим метод первый
 WriteLine();
 MethodCreateOutputArray(array);//Выводим метод второй 
 WriteLine();//Результат работы выводится в консоль при dotnet new console 
+
+
+void MethodCreateInputArray(string[] InputArray) // принимает массив строк 
+{                                                //в качестве аргумента и выводит все его элементы на экран. Она 
+    Write($"InputArray: ");                      //выводит каждый элемент строкой.
+    for (int i = 0; i < InputArray.Length; i++)  
+    {
+        Write($"{InputArray[i]}");
+    }
+}
